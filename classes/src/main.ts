eba9a23a73b484e073c0cc2951c0746c6c063e19
Maxmillian Muiruri <maxmillianmuiruri@gmail.com>
2024-06-16 10:26:37 +0300

    learning about classes in typescript

---
+++ classes/src/main.ts
@@ -0,0 +1,40 @@
+class Coder {
+  secondLang!: string;
+
+  constructor(
+    public readonly name: string,
+    public music: string,
+    private age: number,
+    protected lang: string = "Typescript"
+  ) {
+    this.name = name;
+    this.music = music;
+    this.age = age;
+    this.lang = lang;
+  }
+
+  public getAge() {
+    return `Hello, I'm ${this.age}`;
+  }
+}
+
+const Dave = new Coder("Dave", "Rock", 42);
+console.log(Dave.getAge());
+// console.log(Dave.age)
+// console.log(Dave.lang)
+
+class WebDev extends Coder {
+  constructor(
+    public computer: string,
+    name: string,
+    music: string,
+    age: number
+  ) {
+    super(name, music, age);
+    this.computer = computer;
+  }
+
+  public getLang() {
+    return `I write ${this.lang}`;
+  }
+}
